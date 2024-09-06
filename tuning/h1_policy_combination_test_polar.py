070efa7a0db3d098364060588e60ec591231dcd6
Antonio Langella <a.langella31@studenti.unisa.it>
2024-09-06 11:33:48 +0200

    - [ADD] policy combination test with polar coords - [ADD] video rendering for policy combination experiments

--- tuning/h1_policy_combination_test_polar.py
+++ tuning/h1_policy_combination_test_polar.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 import mujoco
 import mujoco.viewer
 import mediapy as media
+import networkx as nx
 import numpy as np
+import cv2
 import pathlib
 import scipy
 import time
@@ -20,20 +22,25 @@ model.opt.timestep = 0.002
 # data
 data = mujoco.MjData(model)
 # agents
-agent_x = agent_lib.Agent(task_id="H1 Walk", 
+agent_forward = agent_lib.Agent(task_id="H1 Walk", 
                         model=model, 
                         server_binary_path=pathlib.Path(agent_lib.__file__).parent
                         / "mjpc"
                         / "agent_server")
-agent_y = agent_lib.Agent(task_id="H1 Walk",
+agent_right = agent_lib.Agent(task_id="H1 Walk",
+                          model=model,
+                            server_binary_path=pathlib.Path(agent_lib.__file__).parent
+                            / "mjpc"
+                            / "agent_server")
+agent_left = agent_lib.Agent(task_id="H1 Walk",
                           model=model,
                             server_binary_path=pathlib.Path(agent_lib.__file__).parent
                             / "mjpc"
                             / "agent_server")
 # agent_x.set_cost_weights({'Face goal':0.0, 'Posture up': 0.1})
 # agent_y.set_cost_weights({'Face goal':0.0, 'Posture up': 0.1})
-agent_x.set_cost_weights({'Posture up': 0.1})
-agent_y.set_cost_weights({'Posture up': 0.1})
+for agent in [agent_forward, agent_right, agent_left]:
+    agent.set_cost_weights({'Posture up': 0.1})
 
 # Experiment info
 current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
@@ -56,7 +63,7 @@ current_agent = 0
 steps_per_planning_iteration = 10
 i = 0
 goal = np.array([10.0, 10.0])
-obstacle = np.array([3.0, 3.0])
+obstacle = np.array([3.0, 1.0])
 
 def plan_and_get_trajectory(agent):
     for _ in range(10):
@@ -78,8 +85,58 @@ def cost(x):
 X, Y = np.meshgrid(np.linspace(-1, 11, 100), np.linspace(-1, 11, 100))
 z = np.array([cost(np.array([x,y])) for x,y in zip(np.ravel(X), np.ravel(Y))])
 Z = z.reshape(X.shape)
+grad = np.gradient(Z)
 ax = plt.figure().add_subplot(projection='3d')
 ax.plot_surface(X, Y, Z)
+ax = plt.figure().add_subplot()
+ax.quiver(X, Y, -grad[1], -grad[0], angles='xy')
+
+G = nx.generators.grid_2d_graph(100, 100)
+
+print("Adding weights")
+for source,dest,data_dict in G.edges(data=True):
+    data_dict['weight'] = cost(np.array([dest[0]/10.0, dest[1]/10.0]))
+def dist(a, b):
+    (x1, y1) = a
+    (x2, y2) = b
+    x1/=10.0
+    y1/=10.0
+    x2/=10.0
+    y2/=10.0
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+path = nx.astar_path(G, (0, 0), (99, 99), heuristic=dist, weight="weight")
+path_arr = np.array(path) * 0.1
+
+# Plot path as line
+plt.plot(path_arr[:,0], path_arr[:,1])
+
+# Plot path as heatmap
+# path_xy = np.zeros((100,100), dtype=np.uint8)
+# for p in path:
+#     path_xy[p[0], p[1]] = 1
+# path_xy = cv2.dilate(path_xy, np.ones((3,3), np.uint8))
+# plt.imshow(path_xy, origin='lower')
+t = np.arange(path_arr.shape[0])
+fit_x = np.polynomial.polynomial.Polynomial.fit(t, path_arr[:,0], 10)
+fit_y = np.polynomial.polynomial.Polynomial.fit(t, path_arr[:,1], 10)
+fit_x_d = fit_x.deriv()
+fit_y_d = fit_y.deriv()
+plt.plot(fit_x(t), fit_y(t))
+
+def closest_point_on_path(x):
+    x = np.array(x)
+    dist = np.linalg.norm(np.stack((fit_x(t) - x[0], fit_y(t) - x[1]), axis=-1), axis=-1)
+    min_index = np.argmin(dist)
+    tangent_vector = np.array([fit_x_d(min_index), fit_y_d(min_index)])
+    tangent_vector /= np.linalg.norm(tangent_vector)
+    return np.argmin(dist), dist, tangent_vector
+
+def crowdsourcing_cost(x,y,forward_vector):
+    x = np.array([x,y])
+    closest_point, dist, tangent_vector = closest_point_on_path(x)
+    fw_point = np.array([fit_x(closest_point), fit_y(closest_point)]) + 0.5 * forward_vector
+    return dist + np.linalg.norm(x-fw_point) + 10 * np.dot(forward_vector, tangent_vector)
+
 plt.show()
 input("Press Enter to continue...")
 
@@ -89,50 +146,66 @@ with mujoco.viewer.launch_passive(model, data) as viewer, ThreadPoolExecutor() a
     with media.VideoWriter(video_path, fps=video_fps, shape=video_resolution) as video:
         while viewer.is_running():
             
+            forward_vector = np.concatenate([quat_to_forward_vector(data.qpos[3:7]), [0]])
+            
             # set planner state
-            agent_x.set_state(
+            agent_forward.set_state(
                 time=data.time,
                 qpos=data.qpos,
                 qvel=data.qvel,
                 act=data.act,
-                mocap_pos=data.qpos[:3] + np.asarray([10.0, 0.0, 0.0]),
-                mocap_quat=data.mocap_quat,
+                mocap_pos=data.qpos[:3] + forward_vector * 10.0,
+                mocap_quat=data.qpos[3:7],
                 userdata=data.userdata,
             )
-            agent_y.set_state(
+            agent_left.set_state(
                 time=data.time,
                 qpos=data.qpos,
                 qvel=data.qvel,
                 act=data.act,
-                mocap_pos= data.qpos[:3] + np.asarray([0.0, 10.0, 0.0]),
-                mocap_quat=data.mocap_quat,
+                mocap_pos=data.qpos[:3],
+                mocap_quat=rotate_quat(data.qpos[3:7], np.pi/8),
                 userdata=data.userdata,
             )
+            agent_right.set_state(
+                time=data.time,
+                qpos=data.qpos,
+                qvel=data.qvel,
+                act=data.act,
+                mocap_pos=data.qpos[:3],
+                mocap_quat=rotate_quat(data.qpos[3:7], -np.pi/8),
+                userdata=data.userdata,
+            )
+            
             if i % steps_per_planning_iteration == 0:
                 t = time.time()
-                f1 = executor.submit(plan_and_get_trajectory, agent_x)
-                f2 = executor.submit(plan_and_get_trajectory, agent_y)
+                f1 = executor.submit(plan_and_get_trajectory, agent_forward)
+                f2 = executor.submit(plan_and_get_trajectory, agent_left)
+                f3 = executor.submit(plan_and_get_trajectory, agent_right)
                 traj1 = f1.result()
                 traj2 = f2.result()
+                traj3 = f3.result()
                 elapsed = time.time() - t
                 print("Elapsed planning time: ", elapsed)
                 traj1_states = traj1["states"]
                 traj2_states = traj2["states"]
+                traj3_states = traj3["states"]
                 #euler distance from goal
-                traj1_goal_distance = cost(traj1_states[-1,:2]) #cost(data.qpos[:2]+np.asarray([0.5, 0.0])) 
-                traj2_goal_distance = cost(traj2_states[-1,:2]) #cost(data.qpos[:2]+np.asarray([0.0, 0.5]))
-                current_agent = 0 if traj1_goal_distance < traj2_goal_distance else 1
+                traj1_goal_distance = crowdsourcing_cost(traj1_states[-1,0], traj1_states[-1,1], quat_to_forward_vector(traj1_states[-1,3:7]))
+                traj2_goal_distance = crowdsourcing_cost(traj2_states[-1,0], traj2_states[-1,1], quat_to_forward_vector(traj2_states[-1,3:7]))
+                traj3_goal_distance = crowdsourcing_cost(traj3_states[-1,0], traj3_states[-1,1], quat_to_forward_vector(traj3_states[-1,3:7]))
+                current_agent = np.argmin([traj1_goal_distance, traj2_goal_distance, traj3_goal_distance])
 
             if current_agent == 0:
-                agent = agent_x
+                agent = agent_forward
+            elif current_agent == 1:
+                agent = agent_left
             else:
-                agent = agent_y
+                agent = agent_right
+            agent = agent_left
             data.ctrl = agent.get_action(nominal_action=True)
             mujoco.mj_step(model, data)
-            v = quat_to_forward_vector(data.qpos[3:7])
-            v2 = quat_to_forward_vector(rotate_quat(data.qpos[3:7], np.pi/8))
-            data.mocap_pos = data.qpos[:3] + np.asarray([v2[0], v2[1], 0.0])
-            print(f"Step {i} forward: {v}, theta: {np.arctan2(v[1], v[0])}")
+            print(f"Step {i} state: {data.qpos}")
             viewer.sync()
             
             # Render video
